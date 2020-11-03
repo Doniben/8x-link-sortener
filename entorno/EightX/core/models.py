483e56c63d042dc9f1cd7c6954d31d2f16f5c08b
Doniben <doniben@esperanto.co>
2020-11-03 05:06:02 -0500

    [UPDATE] solved three problems with decode_enlace, fechas and get_absolute_orl. No yet works the redirection

--- entorno/EightX/core/models.py
+++ entorno/EightX/core/models.py
@@ -16,7 +16,7 @@ class EnlaceQuerySet(models.QuerySet):
     def decode_enlace(self, codigo):
         decode = Hashids(min_length=4, alphabet='abcdefghijklmnopqrstuvwxyz').decode(codigo)[0]
         self.filter(pk=decode).update(contador=models.F('contador') + 1)
-        return self.filter(pk=decode).first()
+        return self.filter(pk=decode).first().url
 
     #Cantidad de enlaces registrados.
 
@@ -31,15 +31,15 @@ class EnlaceQuerySet(models.QuerySet):
     # la función values obtiene valores en forma de diccionarios.
     # Se le pasa el campo que se quiere llama, que es fecha en este caso.
     # Annotate es una función como la de aggregate que trabaja sobre cada objeto del querySet aplicando una operación que le indiquemos. Aggregate opera sobre un conjunto del queryset para obtener un valor único, se usó para contar el total de los enlaces.
-    # luego identificaremos el valor de julio con la cláusula Sum la cual recibe los parámetros del contador (Que cuenta cantidad de redirecciones de una fecha a otr)
+    # luego identificaremos el valor de noviembre con la cláusula Sum la cual recibe los parámetros del contador (Que cuenta cantidad de redirecciones de una fecha a otr)
     #  y filter con Q, que es para realizar consultas complejas. El primer parámetro de Q es el inicio del contador (filter__gte: >) y 
     #  Cuando ya tenemos nuestra anotación de la fecha de inicio y la fecha final y la suma total de ese rango colocamos un nuevo filtro
     # COn el último filtro se filtra tanto la fecha como el contador por cada enlace que se le pasa a la función fecha.
-    # Se queremos escribir más fechas, basta con escribir una , al final de la primera consulta que es julio y agregar los siguinetes meses
+    # Se queremos escribir más fechas, basta con escribir una , al final de la primera consulta que es noviembre y agregar los siguinetes meses
     def fechas(self, pk):
         return self.values('fecha').annotate(
-            julio=models.Sum('contador', filter=models.Q(
-                filter__gte=datetime.date(2019, 7, 1), filter__Lte=datetime.date(2019, 7, 31)
+            noviembre=models.Sum('contador', filter=models.Q(
+                fecha__gte=datetime.date(2020, 11, 1), fecha__lte=datetime.date(2020, 11, 30)
                 )
                 )
             ).filter(pk=pk)
@@ -73,7 +73,7 @@ class Enlace(models.Model):
     # El método se crea cuando creamos un nuevo modelo. De no ser así, al realizar una consulta desde la terminal o el administrador aparecería error que no permite identificar los objetos.
     # Para visualizarlo se usará el formato "URL + Código"
     def __str__(self):
-        return f"URL: {self.url} Código: {self.codigo}"
+        return f"URL: {self.url} codigo: {self.codigo}"
 
     # el método save crea un nuevo objeto dentro de la base de datos cada vez que se le llama.
     # Generamos el código en base a una llave primaria que se genera en cada enlace. Se debe crear justo despues de crearse la llave primaria.
@@ -81,7 +81,7 @@ class Enlace(models.Model):
     # Se codifica la llave primaria de cada enlace registrado.
     # Django automáticamente declara pk (primary key). Es un identificado único en la bd que se autoincrementa con el registro de cada nuevo elemento
     def save(self, *args, **kwargs):
-        super().save(args, **kwargs)
+        super().save(*args, **kwargs)
         if not self.codigo:
             self.codigo = Hashids(min_length=4, alphabet='abcdefghijklmnopqrstuvwxyz').encode(self.pk)
             self.save()
@@ -92,5 +92,4 @@ class Enlace(models.Model):
     def get_absolute_url(self):
         return reverse('core:detalle', kwargs={'pk': self.pk})
 
-    # Trabajando la lógica en un modelo utilizando querySet (pue sno hay lógica en vistas, solo se trabaja desde los modelos).
-    #
+    # Trabajando la lógica en un modelo utilizando querySet (pues no hay lógica en vistas, solo se trabaja desde los modelos).
