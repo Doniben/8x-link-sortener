1568301b7c633633ab9562f13c339fb3c8de6026
Doniben <doniben@esperanto.co>
2020-11-03 05:02:07 -0500

    [FEATURE] anything changed but is ok

--- entorno/EightX/core/admin.py
+++ entorno/EightX/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Enlace
+# Se usa un decorador y se le pasa el modelo de enlace
+# Se crea una clase 
+# Se pasa la propiedad pass
 
-# Register your models here.
+@admin.register(Enlace)
+class Administrador(admin.ModelAdmin):
+    pass
