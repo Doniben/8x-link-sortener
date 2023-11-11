e82f3f02265133abf9452b2b46138ba5eb6e3c86
Doniben <doniben@esperanto.co>
2023-11-11 12:11:57 -0600

    Change the order for static files with whitenoise

--- entorno/EightX/EightX/settings.py
+++ entorno/EightX/EightX/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_obrsdjyx_9eld4mx!75-5tf@@z%^nlv!o!f$zssk3&k$@(x@='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -119,10 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-WHITENOISE_MAX_AGE = 31536000
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_MAX_AGE = 31536000
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'core', 'static'),
