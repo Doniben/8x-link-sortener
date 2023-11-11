6910839084a5ea6925439a613ca2653d19bd7100
Doniben <doniben@esperanto.co>
2023-11-11 12:11:26 -0600

    Update view with dynamic dates for the redirection counter

--- entorno/EightX/core/views.py
+++ entorno/EightX/core/views.py
@@ -4,6 +4,7 @@ from django.views.generic.base import RedirectView
 from django.views.generic.edit import CreateView
 from .forms import AcortadorForm
 from .models import Enlace
+from datetime import datetime
 
 # Se crea la primera vista y se hereda de CreateView y se le pasan 3 propiedades
 # asignamos el Enlace
@@ -44,7 +45,16 @@ class paginaEnlace(DetailView):
 
     def get_context_data(self, **kwargs):
         contexto = super().get_context_data(**kwargs)
-        contexto['noviembre'] = Enlace.enlaces.fechas(self.kwargs['pk'])[0]['noviembre']
+
+        # Obtén la fecha actual
+        fecha_actual = datetime.now()
+
+        # Formatea la fecha actual para que coincida con el formato de tu modelo
+        fecha_actual_formateada = fecha_actual.strftime('%Y-%m-%d')
+
+        # Accede al valor dinámico según la fecha actual formateada
+        contexto['total_redirecciones'] = Enlace.enlaces.fechas(self.kwargs['pk']).filter(fecha=fecha_actual_formateada).first()['total_redirecciones']
+
         return contexto
 
 
